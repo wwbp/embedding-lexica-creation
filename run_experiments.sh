673a7ec4caeafdcf7a49c55e6c2240410d0790be
jsedoc <jsedoc@users.noreply.github.com>
2021-01-19 14:28:46 -0500

    Delete run_experiments.sh

--- run_experiments.sh
+++
@@ -1,7 +0,0 @@
-echo "" &&
-echo "Running experiments:" &&
-cd modeling &&
-echo "....Running cross-validation." &&
-cd main/crossvalidation && python experiment.py && python ttest.py > ttest.txt &&
-cd ../.. &&
-cd .. && echo "Experiments completed." && echo ""
