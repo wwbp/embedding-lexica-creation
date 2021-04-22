77dce2e8fc6268cc68a1a3b860297057d944d587
Wu Zetian <ztwu_nil@zju.edu.cn>
2021-04-22 16:10:50 -0700

    modified bash files

--- FFN_SVM/SVM/run.sh
+++ FFN_SVM/SVM/run.sh
@@ -1,32 +1,7 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 export PYTHONPATH=$PYTHONPATH:$(pwd)/FFN_SVM
-export PYTHONPATH=$PYTHONPATH:$(pwd)/FFN_SVM/FFN
 
-TASK = $1
-if [ $TASK = "train" ]; then 
-    python FFN_SVM/SVM/main.py \
-        --dataFolder=data \
-        --output_dir=FFN_model \
-        --task=train
-elif [ $TASK = "gen"]; then
-    METHOD = $2
-    if [ $METHOD = "partition" ]; then
-        MASK = $3
-        python FFN_SVM/SVM/main.py \
-            --dataFolder=data \
-            --output_dir=lexica/FFN \
-            --task=generate \
-            --method=$METHOD \
-            --model_dir=FFN_model \
-            --masker=$MASK
-    else
-        python FFN_SVM/SVM/main.py \
-            --dataFolder=data \
-            --output_dir=lexica/FFN \
-            --task=generate \
-            --method=$METHOD \
-            --model_dir=FFN_model
-    fi
-fi
-
-
+#Set up model kind, data folder and output folder here.
+python FFN_SVM/SVM/main.py \
+    --dataFolder=data \
+    --output_dir=lexica/SVM
